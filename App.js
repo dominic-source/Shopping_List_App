e7752e0207738f07e5b7bace8e50cce5aaa239fb
dominic-source <chinonsodomnic@gmail.com>
2022-05-14 12:03:53 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60
+  }
+});
+export default App;
